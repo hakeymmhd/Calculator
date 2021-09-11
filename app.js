5d03aeafa5cb92992def7fed36e58774436c3148
mhdhakeym <mhdhakeym786@gmail.com>
2021-09-11 15:35:18 +0800

    stuck

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 console.log("Testing link")
 
+const calc = {
+    operand_1: 0,
+    operand_2: 0,
+    operator: 0,
+};
+
 $ (() => {
-    console.log("doc ready")
+    // console.log("doc ready")
+    const $statement = $('#Statement');
+    const $button = $('.button').on('click', (e) => {
+        calc.operand_1 =  $(e.target).text();
+        console.log(calc.operand_1);
+    })
+    $statement.append($button);
+    const $button2 = $('.button').on('click', (e) => {
+        calc.operand_2 =  $(e.target).text();
+        console.log(calc.operand_2);
+    })
+    
+  
 })
